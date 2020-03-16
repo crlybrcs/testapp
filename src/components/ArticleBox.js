fe883787777d71eba898894cd526b90eb1c10c2e
crlybrcs <tatyana_@outlook.com>
2020-03-17 00:20:59 +0100

    styles for articles

--- src/components/ArticleBox.js
+++ src/components/ArticleBox.js
@@ -3,38 +3,46 @@ import React from 'react';
 const ArticleBox = () => {
 	return (
 		<div className="box main-box">
-			<div className="box small-box">
+			<div className="box small-box1">
 				<span>
-					<img src="picture.png" width="40px" height="40px" alt="icon-picture" />
+					<img src="image-regular.svg" width="40px" height="40px" className="image-svg" alt="icon-picture" />
 				</span>
 				<h5 className="title-one">Turpis Massa Sed Elementum</h5>
 				<p>
 					Viverra ipsum nunc aliquet bibendum enim facilisis. Bibendum neque egestas congue quisque. Cursus
 					euismod quis viverra nibh cras. Et egestas quis ipsum suspendisse ultrices gravida dictum.
 				</p>
-				<a href="#">READ MORE</a>
+				<a href="#" className="a-color">
+					READ MORE
+				</a>
 			</div>
+
 			<div className="box small-box">
 				<span>
-					<img src="picture.png" width="40px" height="40px" alt="icon-picture" />
+					<img src="image-regular.svg" width="40px" height="40px" className="image-svg" alt="icon-picture" />
 				</span>
 				<h5 className="title-one">Curabitur Vitae Nunc</h5>
 				<p>
 					Viverra ipsum nunc aliquet bibendum enim facilisis. Bibendum neque egestas congue quisque. Cursus
 					euismod quis viverra nibh cras. Et egestas quis ipsum suspendisse ultrices gravida dictum.
 				</p>
-				<a href="#">READ MORE</a>
+				<a href="#" className="a-color">
+					READ MORE
+				</a>
 			</div>
-			<div className="box small-box">
+
+			<div className="box small-box1">
 				<span>
-					<img src="picture.png" width="40px" height="40px" alt="icon-picture" />
+					<img src="image-regular.svg" width="40px" height="40px" className="image-svg" alt="icon-picture" />
 				</span>
 				<h5 className="title-one">Tellus Orci Ac Auctor</h5>
 				<p>
 					Viverra ipsum nunc aliquet bibendum enim facilisis. Bibendum neque egestas congue quisque. Cursus
 					euismod quis viverra nibh cras. Et egestas quis ipsum suspendisse ultrices gravida dictum.
 				</p>
-				<a href="#">READ MORE</a>
+				<a href="#" className="a-color">
+					READ MORE
+				</a>
 			</div>
 		</div>
 	);
